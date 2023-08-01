3f5092508a7dc84f7d40b9dea0c3c1ed27739d2b
Boris Victoria <boris.v.victoria@gmail.com>
2023-08-01 10:47:40 +0800

    create model to pass to controllers

--- src/main/java/com/example/prog3_mco2/Main.java
+++ src/main/java/com/example/prog3_mco2/Main.java
@@ -2,6 +2,7 @@ package com.example.prog3_mco2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,12 +11,19 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+
+        //RegularVendingMachine vm = null;
+        RegularVendingMachine vm = new RegularVendingMachine("k", 7,7);
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        loader.setControllerFactory(controllerClass -> new mainController(vm));
+        Parent root = loader.load();
+        Scene scene = new Scene(root, 600, 500);
         stage.setTitle("Vending Machine Factory");
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
+
     }
 
     public static void main(String[] args) {
