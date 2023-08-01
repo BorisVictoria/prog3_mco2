96c7ccf7e2371196475d2b5cc72e8385456eecad
CloverLePimp <109472279+CloverLePimp@users.noreply.github.com>
2023-08-01 16:25:09 +0800

    Create Bills Class

--- src/main/java/com/example/prog3_mco2/Bills.java
+++ src/main/java/com/example/prog3_mco2/Bills.java
@@ -1,4 +1,15 @@
 package com.example.prog3_mco2;
 
 public class Bills {
+    public Bills(int value)
+    {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    private int value;
+
 }
