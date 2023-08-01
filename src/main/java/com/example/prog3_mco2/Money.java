b9fab0afea9931803975e3228fd42b9b2516f807
Boris Victoria <boris.v.victoria@gmail.com>
2023-08-01 15:40:30 +0800

    redesign money to follow oop

--- src/main/java/com/example/prog3_mco2/Money.java
+++ src/main/java/com/example/prog3_mco2/Money.java
@@ -1,7 +1,71 @@
 package com.example.prog3_mco2;
 
+import java.util.ArrayList;
+
 public class Money
 {
+    int[] money;
+    int total;
+
+    public Money()
+    {
+        money = new int[9];
+        total = 0;
+
+    }
+
+    public void addMoney(int[] deposit)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            money[i] += deposit[i];
+        }
+
+        total = 0;
+        for (int i : new int[]{money[0], money[1] * 5, money[2] * 10, money[3] * 20, money[4] * 50, money[5] * 100, money[6] * 200, money[7] * 500, money[8] * 1000})
+        {
+            total += i;
+        }
+    }
+
+    public boolean removeMoney(int[] withdraw)
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            money[i] -= withdraw[i];
+            if (money[i] < 0)
+                return false;
+        }
+
+        total = 0;
+        for (int i : new int[]{money[0], money[1] * 5, money[2] * 10, money[3] * 20, money[4] * 50, money[5] * 100, money[6] * 200, money[7] * 500, money[8] * 1000})
+        {
+            total += i;
+        }
+
+        return true;
+
+    }
+
+    public void removeAll()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            money[i] = 0;
+        }
+
+        total = 0;
+    }
+
+    public int[] getDenominations()
+    {
+        return money;
+    }
+
+    public int getTotal()
+    {
+        return total;
+    }
 
 
 }
