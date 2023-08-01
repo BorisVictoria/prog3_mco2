59156fb01b3c174e8860238aa9e1289167db4a91
Boris Victoria <boris.v.victoria@gmail.com>
2023-08-01 08:28:35 +0800

    create special vending machine class prototype

---
+++ src/main/java/com/example/prog3_mco2/SpecialVendingMachine.java
@@ -0,0 +1,21 @@
+package com.example.prog3_mco2;
+
+import java.util.ArrayList;
+
+public class SpecialVendingMachine extends RegularVendingMachine
+{
+
+
+    /**
+     * Constructor for objects of class RegularVendingMachine
+     *
+     * @param name        the name of the vending machine
+     * @param numSlots
+     * @param numCapacity
+     */
+    public SpecialVendingMachine(String name, int numSlots, int numCapacity) {
+        super(name, numSlots, numCapacity);
+    }
+
+
+}
