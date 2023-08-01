31078e17e6d07523c586a2d105ded6d5cf110236
Boris Victoria <boris.v.victoria@gmail.com>
2023-08-01 16:38:26 +0800

    refactor register

---
+++ src/main/java/com/example/prog3_mco2/Bill.java
@@ -0,0 +1,17 @@
+package com.example.prog3_mco2;
+
+public class Bill
+{
+    int value;
+
+    public Bill(int value)
+    {
+        this.value = value;
+    }
+
+    public int getValue()
+    {
+
+    }
+
+}
