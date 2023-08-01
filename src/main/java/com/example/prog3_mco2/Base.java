09873df51abe3420593f4f126338e4621c49d4cf
Boris Victoria <boris.v.victoria@gmail.com>
2023-08-02 00:28:20 +0800

    add base and addon inherited from items

---
+++ src/main/java/com/example/prog3_mco2/Base.java
@@ -0,0 +1,18 @@
+package com.example.prog3_mco2;
+
+public class Base extends Item
+{
+
+    /**
+     * Constructor for item with a name, price, description, and calories
+     *
+     * @param name        the name of the item
+     * @param price       the price of the item
+     * @param description the description of the item
+     * @param calories    the calories of the item
+     */
+    public Base(String name, int price, String description, int calories)
+    {
+        super(name, price, description, calories);
+    }
+}
