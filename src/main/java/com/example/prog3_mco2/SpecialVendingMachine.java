b76919bcd820a3a2c109626168d7da74b0a83876
Boris Victoria <boris.v.victoria@gmail.com>
2023-08-02 00:32:45 +0800

    add logic for stocking base and addon items

--- src/main/java/com/example/prog3_mco2/SpecialVendingMachine.java
+++ src/main/java/com/example/prog3_mco2/SpecialVendingMachine.java
@@ -17,5 +17,69 @@ public class SpecialVendingMachine extends RegularVendingMachine
         super(name, numSlots, numCapacity);
     }
 
+    /**
+     * This method stocks a new item to the vending machine
+     *
+     * @param name the name of the item to be stocked
+     * @param price the price of the to be stocked
+     * @param description the description of the item to be stocked
+     * @param calories the calories of the item to be stocked
+     *
+     * @return true if the item was added, false if the item was not added
+     */
+    public boolean stockBase(String name, int price, String description, int calories)
+    {
+
+        Base newItem = new Base(name, price, description, calories);
+
+        if (SLOT_LIST.size() >= numSlots || price < 1 || calories < 1)
+            return false;
+        else
+        {
+            for (int i = 0; i < SLOT_LIST.size(); i++)
+            {
+                if (SLOT_LIST.get(i).getName().equals(name))
+                    return false;
+
+            }
+            SLOT_LIST.add(new Slot(newItem));
+        }
+
+        TRANSACTION_LIST.clear();
+        return true;
+    }
+
+    /**
+     * This method stocks a new item to the vending machine
+     *
+     * @param name the name of the item to be stocked
+     * @param price the price of the to be stocked
+     * @param description the description of the item to be stocked
+     * @param calories the calories of the item to be stocked
+     *
+     * @return true if the item was added, false if the item was not added
+     */
+    public boolean stockAddon(String name, int price, String description, int calories)
+    {
+
+        Addon newItem = new Addon(name, price, description, calories);
+
+        if (SLOT_LIST.size() >= numSlots || price < 1 || calories < 1)
+            return false;
+        else
+        {
+            for (int i = 0; i < SLOT_LIST.size(); i++)
+            {
+                if (SLOT_LIST.get(i).getName().equals(name))
+                    return false;
+
+            }
+            SLOT_LIST.add(new Slot(newItem));
+        }
+
+        TRANSACTION_LIST.clear();
+        return true;
+    }
+
 
 }
